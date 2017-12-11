5741065edf7a72973850fd4959fa33a095f798d7
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-11 15:54:59 -0800

    all stat functions should be working now

--- gregor-sunitha/public/scripts/article.js
+++ gregor-sunitha/public/scripts/article.js
@@ -39,7 +39,7 @@ var app = app || {};
   Article.stats = () => {
     Article.numWordsAll();
     Article.allAuthors();
-    //Article.numWordsByAuthor();
+    Article.numWordsByAuthor();
   };
 
   Article.numWordsAll = () => {
@@ -59,34 +59,15 @@ var app = app || {};
       }, []);
   };
 
-  // NOT WORKING
-  // Article.allAuthors = () => {
-  //   //task: get total of unique author names.
-  //   console.log('inside all authors');
-  //   //return Article.all.map(v => console.log('these are our authors', Article.all[v]['author']));
-  //   console.log('article all authors',Article.all.map( (v, i) => {
-  //     return Article.all[i].author;
-  //   })
-  //   .reduce((prev, cur )=> {
-
-  //   }),['initialValue'])
-    
-  //   return Article.all.map( (v, i) => {return Article.all[i].author;})
-  //   // return Article.all.map( (value, i, arr) => { return arr.indexOf(value) === i; });
-  // };
-
-  Article.numWordsByAuthor = () => {
-    return Article.allAuthors().map(author => {})
-  };
-
-  Article.numWordsByAuthor = () => {
-    var prev;
-    return app.Article.all.map(article => article.author)
-      .reduce(function(result, cur) {
-        if (result.indexOf(cur) === -1) result.push(cur);
-        prev = cur;
-        return result;
-      }, []);
+  Article.numWordsByAuthor = () =>{
+    return Article.allAuthors().map(author => {
+      return {
+        name: author,
+        numWords: Article.all.filter(a => a.author === author) 
+          .map(a => a.body.match(/\b\w+/g).length)
+          .reduce((a,b) => a+b)
+      }
+    })
   };
 
   Article.truncateTable = callback => {
